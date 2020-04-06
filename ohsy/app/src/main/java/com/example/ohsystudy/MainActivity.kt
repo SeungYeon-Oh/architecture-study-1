763ebbd02930282b99684bf99eefa7cfe9067d7a
ohseungyeon <sally37345>
2020-04-07 01:44:57 +0900

    [MD] NaverApi 사용할 때 쓰는 아이디와 비밀번호 git에 추가안하고 외부 파일로 const val로 빼서 사용함

--- ohsy/app/src/main/java/com/example/ohsystudy/MainActivity.kt
+++ ohsy/app/src/main/java/com/example/ohsystudy/MainActivity.kt
@@ -1,18 +1,37 @@
 package com.example.ohsystudy
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
     val TAG = "MainActivity"
 
-    val BASE_URL_NAVER_API = "https://openapi.naver.com/"
-    val CLIENT_ID = getString(R.string.CLIENT_ID)
-    val CLIENT_SECRET = getString(R.string.CLIENT_SECRET)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val api = NaverAPI.create()
+
+        api.getSearchMovies(CLIENT_ID, CLIENT_SECRET, "매드맥스", 10, 1)
+            .enqueue(object : Callback<MovieList> {
+                override fun onResponse(
+                    call: Call<MovieList>,
+                    response: Response<MovieList>
+                ) {
+                    // 성공
+                    Log.d(TAG, "성공 : ${response.raw()}")
+                }
+
+                override fun onFailure(call: Call<MovieList>, t: Throwable) {
+                    // 실패
+                    Log.d(TAG, "실패 : $t")
+                }
+            })
+
     }
 }
--- ohsy/app/src/main/java/com/example/ohsystudy/NavarAPI.kt
+++ ohsy/app/src/main/java/com/example/ohsystudy/NavarAPI.kt
@@ -1,6 +1,11 @@
 package com.example.ohsystudy
 
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient.Builder
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
@@ -14,4 +19,34 @@ interface NaverAPI {
         @Query("display") display: Int? = null,
         @Query("start") start: Int? = null
     ): Call<MovieList>
+
+
+    companion object {
+
+        fun create(): NaverAPI {
+            val httpLoggingInterceptor = HttpLoggingInterceptor()
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+            val headerInterceptor = Interceptor {
+                val request = it.request()
+                    .newBuilder()
+                    .addHeader("X-Naver-Client-Id", CLIENT_ID)
+                    .addHeader("X-Naver-Client-Secret", CLIENT_SECRET)
+                    .build()
+                return@Interceptor it.proceed(request)
+            }
+
+            val client = Builder()
+                .addInterceptor(headerInterceptor)
+                .addInterceptor(httpLoggingInterceptor)
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL_NAVER_API)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(NaverAPI::class.java)
+        }
+    }
 }
