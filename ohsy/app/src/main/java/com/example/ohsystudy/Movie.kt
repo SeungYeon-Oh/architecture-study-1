193d9cdef10371ff3ec2c694c96058a527a684cd
ohseungyeon <sally37345>
2020-04-06 22:07:53 +0900

    [ADD] Movie 데이터 클래스와, Movie 데이터들을 담을 어레이리스트 데이터 클래스를 정의함.

--- ohsy/app/src/main/java/com/example/ohsystudy/Movie.kt
+++ ohsy/app/src/main/java/com/example/ohsystudy/Movie.kt
@@ -1,2 +1,14 @@
 package com.example.ohsystudy
 
+data class MovieList (val items : ArrayList<Movie>)
+
+data class Movie(
+    val title: String,
+    val link: String,
+    val image: String,
+    val subtitle: String,
+    val pubDate: String,
+    val director: String,
+    val actor: String,
+    val usrRating: String
+)
