938c8a7ec8bf7f1748e0f2a75a22dbee306b192c
ohseungyeon <sally37345>
2020-04-06 22:20:03 +0900

    [ADD] MainActivity.kt 에 NaverAPI 를 사용하기 위한 클라이언트 아이디와 비밀번호 변수에 담음 (해당 키는 깃에 추가안하고 따로 관리함)

--- ohsy/app/src/main/java/com/example/ohsystudy/MainActivity.kt
+++ ohsy/app/src/main/java/com/example/ohsystudy/MainActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    val CLIENT_ID = getString(R.string.CLIENT_ID)
+    val CLIENT_SECRET = getString(R.string.CLIENT_SECRET)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
