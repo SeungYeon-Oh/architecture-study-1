6b3ab9a19430a2c948572cb4e7a3cd4cea8384a3
ohseungyeon <sally37345>
2020-04-06 23:13:53 +0900

    [ADD] MainActivity 에 TAG 추가

--- ohsy/app/src/main/java/com/example/ohsystudy/MainActivity.kt
+++ ohsy/app/src/main/java/com/example/ohsystudy/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.ohsystudy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    val TAG = "MainActivity"
+
+    val BASE_URL_NAVER_API = "https://openapi.naver.com/"
     val CLIENT_ID = getString(R.string.CLIENT_ID)
     val CLIENT_SECRET = getString(R.string.CLIENT_SECRET)
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
