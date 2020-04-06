5fb2ae2208ca10b0776e7501fae87d115ca23d8c
ohseungyeon <sally37345>
2020-04-07 01:54:04 +0900

    [ADD] MainActivity 레이아웃의 검색 버튼 눌렀을 때 통신하도록 setOnClickListener 추가함

--- ohsy/app/src/main/java/com/example/ohsystudy/MainActivity.kt
+++ ohsy/app/src/main/java/com/example/ohsystudy/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.ohsystudy
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -17,21 +18,24 @@ class MainActivity : AppCompatActivity() {
 
         val api = NaverAPI.create()
 
-        api.getSearchMovies(CLIENT_ID, CLIENT_SECRET, "매드맥스", 10, 1)
-            .enqueue(object : Callback<MovieList> {
-                override fun onResponse(
-                    call: Call<MovieList>,
-                    response: Response<MovieList>
-                ) {
-                    // 성공
-                    Log.d(TAG, "성공 : ${response.raw()}")
-                }
+        searchBtn.setOnClickListener({
+            api.getSearchMovies(CLIENT_ID, CLIENT_SECRET, "매드맥스", 10, 1)
+                .enqueue(object : Callback<MovieList> {
+                    override fun onResponse(
+                        call: Call<MovieList>,
+                        response: Response<MovieList>
+                    ) {
+                        // 성공
+                        Log.d(TAG, "성공 : ${response.raw()}")
+                    }
 
-                override fun onFailure(call: Call<MovieList>, t: Throwable) {
-                    // 실패
-                    Log.d(TAG, "실패 : $t")
-                }
-            })
+                    override fun onFailure(call: Call<MovieList>, t: Throwable) {
+                        // 실패
+                        Log.d(TAG, "실패 : $t")
+                    }
+                })
+        })
+
 
     }
 }
