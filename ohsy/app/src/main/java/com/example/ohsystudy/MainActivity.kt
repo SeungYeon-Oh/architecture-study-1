a10e9d1cc1a1ed8919ca2d878b23dccffff914f6
ohseungyeon <sally37345>
2020-04-07 22:49:51 +0900

    [FIX] 응답 받은 데이터 gson으로 파싱할 때 에러나는거 고침. json형식으로 먼저 바꾼 다음에, gson으로 파싱해야함.

--- ohsy/app/src/main/java/com/example/ohsystudy/MainActivity.kt
+++ ohsy/app/src/main/java/com/example/ohsystudy/MainActivity.kt
@@ -28,6 +28,7 @@ class MainActivity : AppCompatActivity() {
         searchBtn.setOnClickListener({
             var movieName = searchInput.text.toString()
             Log.d(TAG, "searchInput : $movieName")
+
             api.getSearchMovies(CLIENT_ID, CLIENT_SECRET, movieName, 10, 1)
                 .enqueue(object : Callback<MovieList> {
                     override fun onResponse(
@@ -37,14 +38,20 @@ class MainActivity : AppCompatActivity() {
                         // 성공
                         Log.d(TAG, "성공 : ${response.raw()}")
 
-                        val body = response.body().toString()
+//                        val body = response.body().toString()
+                        val body = response.body()
                         Log.d(TAG, "body : ${body} ")
-                        Log.d(TAG, "body : ${response.body()?.items.toString()} ")
+//                        Log.d(TAG, "body : ${response.body()?.items.toString()} ")
+
                         val gson = GsonBuilder().create()
 
+                        //응답온 데이터를 먼저 json으로 바꿔줘야함
+                        //이걸 해야 com.google.gson.JsonSyntaxException: java.lang.IllegalStateException: Expected BEGIN_OBJECT but was STRING at line 1 column 1 path $
+                        //이 에러가 안 뜸!!
+                        val jsonMovieList = gson.toJson(body)
 
-                        val movieList = gson.fromJson(body, MovieList::class.java)
-
+                        //json으로 바꾼 데이터를 gson으로 파싱함.
+                        val movieList = gson.fromJson(jsonMovieList, MovieList::class.java)
 
                         runOnUiThread {
                             movieRV.adapter = MovieRVAdapter(movieList)
@@ -62,6 +69,7 @@ class MainActivity : AppCompatActivity() {
 
         })
 
-
     }
+
+
 }
